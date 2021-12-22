6e5b27cf0ef0731609a42f4d22148a8ffb9109bc
Inyoung <inyoung.r@gmail.com>
2021-12-22 21:12:37 +0900

    기능 수정: 소셜로그인(백엔드)

--- djangoreact/djangoreact/urls.py
+++ djangoreact/djangoreact/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
--- djangoreact/users/urls.py
+++ djangoreact/users/urls.py
@@ -1,6 +1,15 @@
 from django.urls import include, path
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = 'http://127.0.0.1:3000'
+    client_class = OAuth2Client
+    
 urlpatterns = [
+    path('google/', GoogleLogin.as_view(), name='google_login'),
     path("auth/", include('dj_rest_auth.urls')),  # 권한 요청하는 url은 domain/users/auth/
     path("auth/register", include('dj_rest_auth.registration.urls')) # 회원 가입 url domain/users/auth/register
 ]
