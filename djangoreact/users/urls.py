2b9f308b6d96577f0b1ccc7f222ea8e1260e4423
ChoiYoungHoon <dudgns2865@naver.com>
2021-12-23 11:03:58 +0900

    resolve: gitignore

--- djangoreact/users/urls.py
+++ djangoreact/users/urls.py
@@ -15,3 +15,4 @@ urlpatterns = [
     path('google/', GoogleLogin.as_view(), name='google_login'),
     path("auth/", include('dj_rest_auth.urls')),  # 권한 요청하는 url은 domain/users/auth/
     path("auth/register", include('dj_rest_auth.registration.urls')) # 회원 가입 url domain/users/auth/register
+]
